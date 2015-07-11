cbadcec56a2d29bb09705775ac671e6a25d585b4
Arunav Sanyal <arunav.sanyal91@gmail.com>
2015-07-11 15:46:34 -0700

    Adding code for HoughLinesTransform + disabling gradle native build till i figure out how to introduce external lib dependency

--- src/main/cpp/HelloWorld.cpp
+++
@@ -1,8 +0,0 @@
-// main.cpp
-#include "Hello.h"
-int main(int argc, char ** argv)
-{
-    Hello hello ("Pepito");
-    hello.sayHello(10);
-    return 0;
-}
---
+++ src/main/include/HoughTransform.hpp
@@ -0,0 +1,34 @@
+#ifndef HOUGHTRANSFORM_H
+#define HOUGHTRANSFORM_H
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#if defined(_WIN32) && defined(DLL_EXPORT)
+#define LIB_FUNC __declspec(dllimport)
+#else
+#define LIB_FUNC
+#endif
+
+class LIB_FUNC HoughLinesTransform
+{
+    private:
+
+		// Matrix representations of source, destination and intermediate destination representations of
+		// Images in the process of transforms being applied to it
+		cv::Mat src, dst, cdst;
+		 
+		// place holder for file name
+		char* filename;
+
+		// Apply the hogue transform on the given file
+		void getLinesFromHogueTransform();
+
+	public:
+        
+		// Default constructor for the hogue transform
+		// Accepts file name and contstructs the matrix representation
+		HoughLinesTransform(const char * filename);    
+};
+
+#endif HOUGHTRANSFORM_H
